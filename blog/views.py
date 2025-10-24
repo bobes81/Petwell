0489e16a75904586e817bfb61459bf761d2a3bdd
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-24 10:56:23 +0100

    Add full CRUD views and URLs for BlogPost

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import BlogPost
 from .forms import BlogPostForm
-from django.contrib.auth.decorators import login_required
 
 def blog_list(request):
     posts = BlogPost.objects.all().order_by('-created_at')
@@ -15,7 +16,34 @@ def create_post(request):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
+            messages.success(request, 'New blog post created successfully!')
             return redirect('blog_list')
+        else:
+            messages.error(request, 'Please correct the errors below.')
     else:
         form = BlogPostForm()
     return render(request, 'blog/blog_form.html', {'form': form})
+
+@login_required
+def edit_post(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Post updated successfully!')
+            return redirect('blog_list')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blog/blog_form.html', {'form': form})
+
+@login_required
+def delete_post(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        messages.success(request, 'Post deleted successfully!')
+        return redirect('blog_list')
+    return render(request, 'blog/blog_confirm_delete.html', {'post': post})
