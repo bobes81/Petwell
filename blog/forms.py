134370933fa4c90344e724d12640b4c04eb9e29c
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-24 10:04:17 +0100

    Add BlogPostForm for create and edit posts

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,18 @@ from .models import BlogPost
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter post title...'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 5,
+                'placeholder': 'Write your story here...'
+            }),
+            'image': forms.ClearableFileInput(attrs={
+                'class': 'form-control-file'
+            }),
+        }
