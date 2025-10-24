7be258ab9bbfeff177f6cc2e66666a611ddba883
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-24 10:33:19 +0100

    Fix: added create_post view to resolve Heroku error 500

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import BlogPost
+from .forms import BlogPostForm
+from django.contrib.auth.decorators import login_required
 
 def blog_list(request):
     posts = BlogPost.objects.all().order_by('-created_at')
     return render(request, 'blog/blog_list.html', {'posts': posts})
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog_list')
+    else:
+        form = BlogPostForm()
+    return render(request, 'blog/blog_form.html', {'form': form})
