9918e2dbaa6b0e0def5a8ee49e23dbe1188a8062
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-25 06:34:29 +0100

    Add ROOT_URLCONF and full Django settings for Heroku

--- petwell/settings.py
+++ petwell/settings.py
@@ -10,6 +10,58 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+# ------------------------------
+# Core Django settings
+# ------------------------------
+ROOT_URLCONF = 'petwell.urls'
+WSGI_APPLICATION = 'petwell.wsgi.application'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'crispy_forms',
+    'blog',
+    'products',
+    'orders',
+    'account',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+SITE_ID = 1
 
 # ------------------------------
 # Database configuration (Heroku PostgreSQL)
@@ -22,7 +74,6 @@ DATABASES = {
     )
 }
 
-
 # ------------------------------
 # Static & Media files
 # ------------------------------
@@ -32,7 +83,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # ------------------------------
 # Email settings (SendGrid)
