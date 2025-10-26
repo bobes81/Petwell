84399b1a443f0dc8aca0b12bcd387c202ae9a489
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-26 11:25:40 +0000

    Increase image field max_length to 255 for Cloudinary URLs

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    image = CloudinaryField('image', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True, max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
