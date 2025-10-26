9d6a45848f09646f8a675b74f33338d81adb266f
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-26 08:56:14 +0000

    Fix: add authentication redirect URLs to prevent 404 after signup

--- petwell/settings.py
+++ petwell/settings.py
@@ -86,3 +86,17 @@ EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'ivan.kimpl@email.cz'
+# ------------------------------
+# Authentication redirects
+# ------------------------------
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_SIGNUP_REDIRECT_URL = '/'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/'
+# ------------------------------
+# Authentication redirects
+# ------------------------------
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_SIGNUP_REDIRECT_URL = '/'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/'
