c985b48ed07cc395ece932517e6ae98f3a4be30b
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-26 15:45:49 +0000

    Set home.html as homepage and move blog to /blog/

--- petwell/urls.py
+++ petwell/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('blog/', include('blog.urls', namespace='blog')),
     path('account/', include('allauth.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
