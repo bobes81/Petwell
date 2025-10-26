91c16f7f7252efd62661024e2af07e73dc710dac
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-26 16:27:43 +0000

    Add Product model and admin registration

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock', 'created_at')
+    list_display = ('name', 'price', 'created_at')
     search_fields = ('name',)
--- products/models.py
+++ products/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=120)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    stock = models.PositiveIntegerField(default=0)
-    image_url = models.URLField(blank=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
