1cd72ca0db0aaa7c7b313d5b07ea3a332bc98770
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-26 11:12:49 +0000

    Fix BlogPost model: rename created_on to created_at

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,11 @@ class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    image = CloudinaryField('image', default='placeholder')
-    created_on = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField('image', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
