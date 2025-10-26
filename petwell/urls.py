0cab3ab0dd72fb7a421b08d97e9964610dc71118
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-26 15:40:57 +0000

    Link cart and checkout apps in main URLconf

--- petwell/urls.py
+++ petwell/urls.py
@@ -1,24 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('blog/', include('blog.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('', include('blog.urls')),
+    path('account/', include('allauth.urls')),
+    path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-from django.conf.urls import handler404
-from django.shortcuts import render
-
-def custom_404(request, exception):
-    return render(request, 'errors/404.html', status=404)
-
-handler404 = custom_404
-
