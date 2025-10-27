10d90c65a02248a4bf583626420b406efb712315
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 11:41:38 +0000

    Fix SEO routes for robots.txt and sitemap.xml

--- petwell/urls.py
+++ petwell/urls.py
@@ -1,34 +1,28 @@
+cat > petwell/urls.py << 'EOF'
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
-    path('account/', include('allauth.urls')),
+    path('', include('blog.urls'), name='home'),
+    path('account/', include('accounts.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('products/', include('products.urls')),
+    path(
+        'robots.txt',
+        TemplateView.as_view(
+            template_name='robots.txt',
+            content_type='text/plain'
+        ),
+    ),
+    path(
+        'sitemap.xml',
+        TemplateView.as_view(
+            template_name='sitemap.xml',
+            content_type='application/xml'
+        ),
+    ),
 ]
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.views.static import serve
-from django.conf import settings
-from django.urls import re_path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("blog.urls"), name="home"),
-    path("account/", include("accounts.urls")),
-    path("cart/", include("cart.urls")),
-    path("checkout/", include("checkout.urls")),
-    path("products/", include("products.urls")),
-]
-
-# Serve robots.txt and sitemap.xml from static/
-urlpatterns += [
-    re_path(r"^robots\.txt$", serve, {"path": "robots.txt", "document_root": settings.STATIC_ROOT}),
-    re_path(r"^sitemap\.xml$", serve, {"path": "sitemap.xml", "document_root": settings.STATIC_ROOT}),
-]
+EOF
