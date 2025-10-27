148057805f3ddee91d293e8187622b311035b79c
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 05:16:54 +0000

    Fix product image field for Cloudinary

--- products/models.py
+++ products/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=120)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['name']
 
     def __str__(self):
         return self.name
