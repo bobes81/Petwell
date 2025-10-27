5a23aaf348a36dc7a23310e863c7fce80f79532c
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 15:40:10 +0000

    Fix missing allauth URLs (login/logout/signup)

--- petwell/urls.py
+++ petwell/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('account/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),  # important as this adds login/logout/signup
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('products/', include('products.urls')),
