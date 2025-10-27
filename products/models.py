ff5e91a166886c3a87ffadba3ec6ffb1b199912c
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 05:43:46 +0000

    Switch product images to CloudinaryField

--- products/models.py
+++ products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
 
     def __str__(self):
         return self.name
