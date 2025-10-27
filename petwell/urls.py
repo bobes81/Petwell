d375a3a089e5f47743204fa45e71ee2fe76c71d5
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 11:46:15 +0000

    Move robots.txt and sitemap.xml to static for SEO

--- petwell/urls.py
+++ petwell/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.generic import TemplateView
 from django.views.static import serve
 from django.conf import settings
 
@@ -11,6 +10,6 @@ urlpatterns = [
     path("cart/", include("cart.urls")),
     path("checkout/", include("checkout.urls")),
     path("products/", include("products.urls")),
-    re_path(r"^robots\.txt$", serve, {"path": "robots.txt", "document_root": settings.BASE_DIR / 'templates'}),
-    re_path(r"^sitemap\.xml$", serve, {"path": "sitemap.xml", "document_root": settings.BASE_DIR / 'templates'}),
+    re_path(r"^robots\.txt$", serve, {"path": "robots.txt", "document_root": settings.STATIC_ROOT}),
+    re_path(r"^sitemap\.xml$", serve, {"path": "sitemap.xml", "document_root": settings.STATIC_ROOT}),
 ]
