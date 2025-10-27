8b3d7a8f06906b2ea2740f5c11dd4576f8933e90
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 11:37:47 +0000

    Add static serving for robots.txt and sitemap.xml

--- petwell/urls.py
+++ petwell/urls.py
@@ -11,3 +11,24 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('products/', include('products.urls')),
 ]
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.views.static import serve
+from django.conf import settings
+from django.urls import re_path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls"), name="home"),
+    path("account/", include("accounts.urls")),
+    path("cart/", include("cart.urls")),
+    path("checkout/", include("checkout.urls")),
+    path("products/", include("products.urls")),
+]
+
+# Serve robots.txt and sitemap.xml from static/
+urlpatterns += [
+    re_path(r"^robots\.txt$", serve, {"path": "robots.txt", "document_root": settings.STATIC_ROOT}),
+    re_path(r"^sitemap\.xml$", serve, {"path": "sitemap.xml", "document_root": settings.STATIC_ROOT}),
+]
