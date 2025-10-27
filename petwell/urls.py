ce24c65567e59821ebf912c861876d4e5db35d3d
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 18:21:00 +0000

    Add TemplateView for robots.txt and sitemap.xml

--- petwell/urls.py
+++ petwell/urls.py
@@ -11,3 +11,18 @@ urlpatterns = [
     path('checkout/', include(('checkout.urls', 'checkout'), namespace='checkout')),
     path('accounts/', include('allauth.urls')),
 ]
+# --- Robots.txt and Sitemap.xml ---
+from django.views.generic import TemplateView
+
+urlpatterns += [
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+        name="robots",
+    ),
+    path(
+        "sitemap.xml",
+        TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml"),
+        name="sitemap",
+    ),
+]
