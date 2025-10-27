b42a20e1200910c89f7e2baf3dcb632f0855c9ed
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 05:18:19 +0000

    Fix ProductAdmin: remove created_at field

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,5 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'created_at')
+    list_display = ('name', 'price')  # removed 'created_at'
     search_fields = ('name',)
