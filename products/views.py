21304f53bfd18651db884f7ab4884a0d440ddfcd
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 18:47:27 +0000

    Fix template path for product_list view

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
 
 def product_list(request):
     products = Product.objects.all()
-    return render(request, "products/products_list.html", {"products": products})
+    return render(request, "products/product_list.html", {"products": products})
