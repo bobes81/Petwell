b1ed13c4d7a6f6cc39c7c96213c3e35ecef14210
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 12:06:25 +0000

    Add product listing page

--- products/views.py
+++ products/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from .models import Product
 
 def product_list(request):
-    """Display all products."""
     products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+    return render(request, "products/products_list.html", {"products": products})
