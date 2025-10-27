add61230c4080c630e4ef6b6984feb63c27d408a
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 14:43:47 +0000

    Restore /blog/ prefix and fix blog route

--- petwell/urls.py
+++ petwell/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from petwell.views import robots_txt, sitemap_xml
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
-    path("account/", include("accounts.urls")),
-    path("cart/", include("cart.urls")),
-    path("checkout/", include("checkout.urls")),
-    path("products/", include("products.urls")),
-    path("robots.txt", robots_txt),
-    path("sitemap.xml", sitemap_xml),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('account/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
+    path('products/', include('products.urls')),
+    path('robots.txt', include('robots.urls')),
+    path('sitemap.xml', include('sitemap.urls')),
 ]
