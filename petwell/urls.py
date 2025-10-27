ab7eb8c8cdc55dbd4f0c53a6bf6a1b34b54b3ed1
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-27 17:35:30 +0000

    Restore homepage route to index.html

--- petwell/urls.py
+++ petwell/urls.py
@@ -1,16 +1,13 @@
-from blog.views_home import home
-from blog.home_view import home
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("", home, name="home"),
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('account/', include('accounts.urls')),
-    path('accounts/', include('allauth.urls')),  # important as this adds login/logout/signup
-    path('cart/', include('cart.urls')),
-    path('checkout/', include('checkout.urls')),
-    path('products/', include('products.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('products/', include(('products.urls', 'products'), namespace='products')),
+    path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
+    path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
+    path('checkout/', include(('checkout.urls', 'checkout'), namespace='checkout')),
+    path('accounts/', include('allauth.urls')),
 ]
