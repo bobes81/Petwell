4467d2fd759f0a22522e89b92d0fd6240cc05e7a
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-28 11:46:56 +0000

    Fix duplicated urlpatterns and restore blog_list route

--- blog/urls.py
+++ blog/urls.py
@@ -4,17 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('subscribe/', views.subscribe, name='subscribe'),    path('', views.blog_list, name='blog_list'),
+    path('', views.blog_list, name='blog_list'),
     path('post/<int:pk>/', views.blog_detail, name='blog_detail'),
     path('new/', views.create_post, name='create_post'),
     path('post/<int:pk>/edit/', views.edit_post, name='edit_post'),
     path('post/<int:pk>/delete/', views.delete_post, name='delete_post'),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
     path('subscribe/', views.subscribe, name='subscribe'),
 ]
-
