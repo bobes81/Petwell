38005007ce0a7ef0d2ba657c12423341548a1188
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-28 04:34:42 +0000

    Fix ALLOWED_HOSTS for Heroku 404 page

--- petwell/settings.py
+++ petwell/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "replace-this-with-a-random-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["petwell-app-ivo.herokuapp.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
