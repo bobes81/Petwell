2eb4a7f36e27d9b2e300d44d511cbc23c8c2e48a
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-28 14:10:50 +0000

    Create Subscriber table manually

---
+++ blog/migrations/0003_create_subscriber_table.py
@@ -0,0 +1,19 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_create_subscriber_table'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Subscriber',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('email', models.EmailField(max_length=254, unique=True)),
+                ('subscribed_at', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
